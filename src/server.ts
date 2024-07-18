1c00598457f0adc678b802c55521ffacfd9de815
k1nnyyY <osman.haciev231@gmail.com>
2024-07-18 15:14:25 +0400

    add

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 
 dotenv.config();
 
-const adminPasswordFile = path.join(__dirname, "admin_passwords.txt");
+const adminPasswordFile = path.join("../admin_passwords.txt");
 
 let adminPasswords: string[] = [];
 
